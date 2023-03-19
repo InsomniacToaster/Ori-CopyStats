7bb0faace1218e8baef10e5016555f2f49bc4456
InsomniacToaster <89503619+InsomniacToaster@users.noreply.github.com>
2023-03-19 13:06:53 -0600

    It works!
    Successful file copy through OBS, still need to put in more logic for checking if files exist.

--- ori-copystats.py
+++ ori-copystats.py
@@ -1,12 +1,12 @@
+# look into marshalling
 import shutil
-import obspython as S
+import obspython as OBS
 
-prop_oride_path = ''
-prop_destination_path = ''
-randomizer_path = prop_oride_path
-destination_path = prop_destination_path
+randomizer_path = ''
+destination_path = ''
 seed_file_name = 'randomizer.dat'
 stats_file_name = 'stats.txt'
+hotkey_id = OBS.OBS_INVALID_HOTKEY_ID
 
 # Description displayed in the Scripts dialog window
 def script_description():
@@ -14,32 +14,52 @@ def script_description():
             Copies the randomizer.dat and stats.txt into a location of your choosing."""
 
 def script_properties():
-    props = S.obs_properties_create()
-    S.obs_properties_add_path(props, 'prop_oride_path', 'Ori DE File Path', S.OBS_PATH_DIRECTORY, '', 'C:/Program Files (x86)/Steam/steamapps/common/Ori DE')
-    S.obs_properties_add_path(props, "prop_destination_path", "File Destination Path", S.OBS_PATH_DIRECTORY, '', None)
+    props = OBS.obs_properties_create()
+    OBS.obs_properties_add_path(props, "randomizer_path", 'Ori DE File Path', OBS.OBS_PATH_DIRECTORY, '', 'C:/Program Files (x86)/Steam/steamapps/common/Ori DE')
+    OBS.obs_properties_add_path(props, "destination_path", 'File Destination Path', OBS.OBS_PATH_DIRECTORY, '', None)
     return props
 
 def script_load(settings):
-    prop_oride_path = S.obs_data_get_string(settings, 'prop_oride_path')
-    prop_destination_path = S.obs_data_get_string(settings, 'prop_destination_path')
+    global randomizer_path
+    global destination_path
+    
+    randomizer_path = OBS.obs_data_get_string(settings, "randomizer_path")
+    destination_path = OBS.obs_data_get_string(settings, "destination_path")
+    OBS.script_log(OBS.LOG_INFO, randomizer_path)
+    OBS.script_log(OBS.LOG_INFO, destination_path)
 
-def copy_files():
+    # set hotkey
+    global hotkey_id
+    hotkey_id = OBS.obs_hotkey_register_frontend(
+        script_path(), "Ori CopyStats", copystats_copy_files)
+    hotkey_save_array = OBS.obs_data_get_array(
+        settings, "oricopystats_hotkey")
+    OBS.obs_data_array_release(hotkey_save_array)
 
-    ## Parses the seed and stat files and stores the seed name for the files in variables.
+def script_save(settings):
+    # save hotkey
+    hotkey_save_array = OBS.obs_hotkey_save(hotkey_id)
+    OBS.obs_data_set_array(settings, "oricopystats_hotkey", hotkey_save_array)
+    OBS.obs_data_array_release(hotkey_save_array)
 
-    with open((randomizer_path + seed_file_name), 'r', encoding='utf-8') as f:
+def copystats_copy_files():
+
+    # Parses the seed and stat files and stores the 
+    # seed name for the files in variables.
+
+    with open((randomizer_path + '\\' + seed_file_name), 'r', encoding='utf-8') as f:
         seed_headers = f.readline()
     seed_headers=seed_headers.strip().replace('|' , ',')
     seed_headers=seed_headers.split(",")
     seed_headers = (seed_headers[-1])
 
-    with open((randomizer_path + stats_file_name), 'r', encoding='utf-8') as f:
+    with open((randomizer_path + '\\' + stats_file_name), 'r', encoding='utf-8') as f:
         stats_headers = f.readline()
     stats_headers=stats_headers.strip().replace('|' , ',')
     stats_headers=stats_headers.split(",")
     stats_headers = (stats_headers[-1])
 
     ## Copy the seeds and stats to the specified directory
-    shutil.copy2((randomizer_path + seed_file_name) , (destination_path + seed_headers + '-' +  seed_file_name))
-    shutil.copy2((randomizer_path + stats_file_name) , (destination_path + stats_headers + '-' + stats_file_name))
+    shutil.copy2((randomizer_path + '\\' + seed_file_name) , (destination_path + '\\' + seed_headers + '-' +  seed_file_name))
+    shutil.copy2((randomizer_path + '\\' + stats_file_name) , (destination_path + '\\' + stats_headers + '-' + stats_file_name))
 
